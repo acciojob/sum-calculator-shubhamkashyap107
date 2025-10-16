389f787eb9bd99e5dcaea6794316de65d8bb893a
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-10-16 14:33:51 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const[sum, setSum] = useState(0)
+  const[curr, setCurr] = useState(0)
+
+  useEffect(() => {
+    setSum(sum + Number(curr))
+  }, [curr])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" onChange={(e) => {
+          setCurr(e.target.value)
+        }} />
+        <p>Sum: {sum}</p>
     </div>
   )
 }
